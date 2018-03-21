3936974d3a1e85844434bdd8a35c4e0735b28099
Sergii <sergiigrytsaienko@gmail.com>
2018-03-20 22:57:26 -0500

    tests&view update

--- app/Thread.php
+++ app/Thread.php
@@ -4,7 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Thread extends Model
-{
-    //
+class Thread extends Model {
+	public function path() {
+		return '/laravel_forum/public/threads/' . $this->id; // todo FIX ROUTING!!
+	}
+
+	public function replies() {
+		return $this->hasMany( Reply::class );
+	}
+
 }
